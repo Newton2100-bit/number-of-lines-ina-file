5595df32e8dc27bdacf2843b737c9dd7e9f49c06
newton irungu <mwauranewton99@gmail.com>
2001-01-01 06:42:43 +0300

    A c program to count lines in a file

---
+++ number_of_lines.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+static int count = 0, rd;
+static FILE *fileptr;
+static char character;
+
+void openfile(char *);
+void count_lines();
+
+int main(int argc, char **argv){
+	// pthread_t nchars, nwords;
+	/* These wiil count the number of lines in a file and 
+	   printf out the number */
+
+	if(argc < 2){
+		fprintf(stderr, "Kindly provide the file name.\n");
+		exit(0);
+	}
+	openfile(argv[1]);
+	count_lines();
+
+
+	printf("[ %s ]\n\tlines : %d\n" , argv[1], count);
+	fclose(fileptr);
+	return 0;
+}
+
+void openfile(char *filename){
+	fprintf(stderr, "opening file %s\n", filename);
+	if((fileptr = fopen(filename, "r")) == NULL){
+		fprintf(stderr, "could not ooen the file %s\n", filename);
+		exit(0);
+	}
+	printf("file succesfully opened\n");
+}
+
+
+void count_lines(){
+	do{
+		rd = fscanf(fileptr, "%*[^\n]%c", &character);
+		printf("read %c\t", character);
+		if(rd == EOF)
+			break;
+		else
+			count++;
+#if 0
+		if(rd >= 1 /* && ((int)character != -1)*/) count++;
+		else break;
+
+		/* could have used feof() to detect end of file but avoided 
+		 * the call stack overhead */
+#endif
+	}while(1);
+}
